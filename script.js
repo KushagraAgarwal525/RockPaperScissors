35a841380d2d0e8a518a80f872330d0b647df7b1
Kushagra <kushagraagarwal525.ka@gmail.com>
2022-03-06 18:07:26 +0530

    Add playRound function

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function ComputerPlay() {
     let randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     return options[randomnumber];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    let a;
+    switch (true) {
+        case playerSelection == computerSelection.toUpperCase():
+            a = "Tie";
+            break;
+        case playerSelection === "ROCK" && computerSelection === "Scissors":
+            a =  `${playerSelection} crushes ${computerSelection}`;
+            break;
+        case playerSelection === "ROCK" && computerSelection === "Paper":
+            a = `${computerSelection} captures ${playerSelection}`;
+            break;
+        case playerSelection === "PAPER" && computerSelection === "Scissors":
+            a = `${computerSelection} cut ${playerSelection}`;
+            break;
+        case playerSelection === "PAPER" && computerSelection === "Rock":
+            a = `${playerSelection} captures ${computerSelection}`;
+            break;
+        case playerSelection === "SCISSORS" && computerSelection === "Rock":
+            a = `${computerSelection} crush ${playerSelection}`;
+            break;
+        case playerSelection === "SCISSORS" && computerSelection === "Paper":
+            a = `${playerSelection} cut ${computerSelection}`;
+            break;
+    }
+    return a;
+}
